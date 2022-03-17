30b409b43eaefd2592dd75824e45b8cd47b54afb
Barney <holyja@naver.com>
2022-03-17 20:00:32 +0900

    style: Fix typo(ninkname -> nickname)

--- users/views.py
+++ users/views.py
@@ -9,12 +9,15 @@ from users.models       import User
 class KakaoLogInView(View):
     def get(self, request):
         try:
-            kakao_access_token  = request.headers.get('Authorization')
-            kakao_user_info_api = 'https://kapi.kakao.com/v2/user/me'            
+            kakao_access_token  = request.headers.get('Authorization')           
+            kakao_user_info_api = 'https://kapi.kakao.com/v2/user/me'
             user_info_response  = requests.get(kakao_user_info_api, headers={'Authorization' : f'Bearer {kakao_access_token}'}, timeout=2).json()
-            
+
+            if user_info_response.get('code') == -401:
+                return JsonResponse({'message' : 'INVALID_TOKEN'}, status = 401)
+
             kakao_id = user_info_response['id']
-            name     = user_info_response['properties']['ninkname']
+            name     = user_info_response['properties']['nickname']
             email    = user_info_response['kakao_account']['email']
             
             user, is_created = User.objects.get_or_create(
@@ -28,4 +31,3 @@ class KakaoLogInView(View):
         
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
-
